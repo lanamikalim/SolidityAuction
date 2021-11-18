f20c0a0dc50ba0cd00d2def6879a35e197b8389b
lanamikalim <47696234+lanamikalim@users.noreply.github.com>
2021-11-17 19:35:55 -0800

    Added Bid function, modified transfNFT[Untested]
    -bid function uses mapping to store address and respective bid
    -updateTopBid function updates topbid and topbidder every time a new bid is placed
    -transferNFT no longer has owner paramter, and by extension close auction no longer has owner parameter.
    -NFTowner is parameter given when constructed
    -variable names updated to be more descriptive

--- LocalAuction/LocalAuction.sol
+++ LocalAuction/LocalAuction.sol
@@ -2,45 +2,56 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract Auction2{
     address payable public beneficiaryAddress;
-    address payable public owner;
+    address public nftOwner;
     uint public auctionClose;
     uint256 public testnum;
      IERC721 public nft;
     uint public nftId;
      address public topBidder;
     uint public topBid;
-    
-     function Auction( uint biddingTime, address payable beneficiary,address _nft,
-        uint _nftId) public{
+    mapping (address => uint) public allBids;
+    uint public maxBids;
+    uint public amountOfBids;
+
+     function Auction(address payable beneficiary, address nftowner, address _nft, uint _nftId, uint maxbids) public{
         beneficiaryAddress = beneficiary;
         nft = IERC721(_nft);
         nftId = _nftId;
-        auctionClose = block.timestamp + biddingTime;
+       // auctionClose = block.timestamp + biddingTime;
         testnum = 0;
+        nftOwner= nftOwner;
+        maxBids = maxbids;
+    }
+ 
+     
+    //lets user bid an amount, if amount of bids is greater than 
+     function bid(uint bidAmount, address bidder) public{
+        updateTopBid(bidAmount,bidder);
+        allBids[bidder] = bidAmount;
+        amountOfBids++;
+        if(amountOfBids == maxBids){
+          closeAuction();
+        }
+    }
+    
+    function updateTopBid(uint bidAmount,address bidder){
+        if(bidAmount >topBid){
+            topBidder = bidder;
+        }
         
     }
-    /**
-     * Note: input address of dog nft is invalid when constructing SimpleAuction)
-     * nft: 0x13066ee900a8c4e2c9cd7ce0096adf9b907d0cff
-     * */
-  
     function closeAuction() public{
         require(block.timestamp >= auctionClose);
-      
-        
+        transferNFT();
     }
     
-    function bid() public{
-        //CHajkjfhjkhdajkfdjkthe 
-        //AHKFkjaskjfkjhasjkhfashkj
-    }
-
     
-    function transferNFT(address owner) public payable{
-        //msg.sender
-        //0x2ad7aa2a14a761c4e2ef3089d6dccb204afc3534
+//Setup Note: -run  remixd -s . --remix-ide https://remix.ethereum.org/ in github directory before connecting to localhost
+//Runtime notes: -deploy thingy - create nft with the contract address as the owner -use constructor on contract, put my address as beneficiaryAddress -transferNFT with contract address as owner
+    
+    function transferNFT() public payable{
         //Okay this is now working yay
-        nft.safeTransferFrom(owner,beneficiaryAddress,nftId);
+        nft.safeTransferFrom(nftOwner,beneficiaryAddress,nftId);
 
     }
     
